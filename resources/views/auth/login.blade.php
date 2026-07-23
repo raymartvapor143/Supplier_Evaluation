690dc4f4fad999da452227c9a49f318014045d0c
Raymart Vapor <raymart.vapor1111@gmail.com>
2026-07-23 15:25:43 +0800

    icon for login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,6 +3,7 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="icon" type="image/x-icon" href="{{ asset('logo.png') }}">
   <meta name="csrf-token" content="{{ csrf_token() }}">
 
   <title>Login</title>
